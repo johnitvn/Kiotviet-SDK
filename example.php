210b70dfd01f5d804a62f67f07c0e8ffa5433f01
John Martin <john.itvn@gmail.com>
2020-10-19 17:15:10 +0700

    Update example

--- example.php
+++ example.php
@@ -8,15 +8,27 @@ use GuzzleHttp\Client;
 
 if (file_exists('example_private.php')) {
     include 'example_private.php';
-
 } else {
     define('ID', '');
     define('SECRET', '');
     define('RETAILER', '');
-    
 }
 
-$config =  new Config(ID,SECRET,RETAILER,'https://id.kiotviet.vn','/connect/token');
+
+$config =  new Config(ID,SECRET,RETAILER);
 $client = new KVClient($config);
 $token =  $client->getAccessToken();
 echo $token;
+
+
+$response = $client->get('categories',[
+    'format'=>'json',
+    'orderBy'=>'modifiedDate',
+    'orderDirection'=>'asc',
+    'pageSize'=>100,
+    'includerRemoveIds' => 'true',
+    'lastModifiedFrom'=>'2000-01-01'
+]);
+
+var_dump($response);
+
